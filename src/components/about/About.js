ad5106c20f1f2f0371f5cb297b126c3b9d450d59
Marie Helen Berger <44783768+mie91@users.noreply.github.com>
2020-04-11 17:59:38 +0200

    changed some styles in bg

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Container, Col, Row } from "react-bootstrap";
 
 function About() {
     return (
-        <h1>About</h1>
+        <div className="about-bg">
+            <Container>
+            <h1>About</h1>
+
+                <div>Esse eu elit culpa in incididunt magna velit deserunt laboris elit qui magna eu. Dolore cupidatat magna eiusmod fugiat duis ad in ipsum consequat exercitation adipisicing dolor anim id. Nisi laboris cillum amet in nisi. Culpa nisi anim magna nisi minim aute sunt nostrud anim nisi. Ex minim esse do labore minim nisi ipsum ad dolore. Labore reprehenderit aliqua aute mollit consectetur velit velit pariatur non. Reprehenderit sunt duis sunt culpa ipsum mollit labore do exercitation consequat laboris in adipisicing.
+                </div>
+            </Container>
+        </div>
     );
 }
 
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Container, Col, Row } from "react-bootstrap";
 
 function Contact() {
     return (
-        <h1>Contact</h1>
+        <div className="contact-bg">
+            <Container>
+                <h1>Contact</h1>
+
+                <div>Ea sint nulla laborum minim nisi adipisicing ut laboris. Cillum aute reprehenderit ex aute anim pariatur aliquip do proident. Nisi ipsum duis esse nulla reprehenderit eiusmod culpa et.
+
+                </div>
+            </Container>
+        </div>
     );
 }
 
